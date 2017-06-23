13e2733dcf664f7ab14af904eeae10280ad581c4
erzh <cmpan@qq.com>
2017-06-23 13:33:36 +0800

    增加接口StorageInterface

--- lib/StorageAbstract.php
+++ lib/StorageAbstract.php
@@ -20,7 +20,7 @@ namespace wf\storage;
  * @link        http://docs.windwork.org/manual/wf.storage.html
  * @since       0.1.0
  */
-abstract class StorageAbstract
+abstract class StorageAbstract implements StorageInterface
 {
     /**
      * 
@@ -73,7 +73,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 获取附件存贮站点网址
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getStorageUrl()
      */
     public function getStorageUrl()
     {
@@ -81,12 +82,8 @@ abstract class StorageAbstract
     }
             
     /**
-     * 获取附件的路径
-     * 使用两种路径：相对路径和完整路径
-     * 当程序设置使用完整路径的时候就全部使用完整路径，否则在PHP脚本中用header跳转的，一律用完整路径，前端页面用相对路径
-     * 
-     * @param string $path
-     * @return string
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getUrl()
      */
     public function getUrl($path)
     {
@@ -97,9 +94,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 从URL获取文件存贮的path
-     * @param string $url
-     * @return string
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getPathFromUrl()
      */
     public function getPathFromUrl($url)
     {
@@ -109,10 +105,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 获取附件完整网址
-     * 
-     * @param string $path
-     * @return string
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getFullUrl()
      */
     public function getFullUrl($path)
     {
@@ -129,11 +123,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 获取上传文件（在存贮介质上）的真实路径 {$wrapper}{$path}，实际文件不存在时也有值返回
-     * 
-     * @param string $path
-     * @param string $type 
-     * @return string
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getRealPath()
      */
     public function getRealPath($path)
     {
@@ -146,12 +137,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 获取缩略图相对URL，在显示图片时使用
-     *
-     * @param int $imgId
-     * @param int $width
-     * @param int $height
-     * @return string
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getThumbUrl()
      */
     public function getThumbUrl($path, $width = 100, $height = 100)
     {
@@ -169,8 +156,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 删除附件
-     * @param string $path
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::remove()
      */
     abstract public function remove($path);
     
@@ -182,17 +169,14 @@ abstract class StorageAbstract
     abstract public function removeThumb($path);
     
     /**
-     * 删除所有缩略图
-     *
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::clearThumb()
      */
     abstract public function clearThumb();
     
     /**
-     * 获取缩略图路径
-     * 
-     * @param string $path
-     * @param int $width
-     * @param int $height
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getThumbPath()
      */
     public function getThumbPath($path, $width, $height)
     {
@@ -206,10 +190,8 @@ abstract class StorageAbstract
     }
     
     /**
-     * 文件上传的附件是否是保存为静态文件，
-     * 建议可在继承类中覆盖该方法
-     * 
-     * @return boolean
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::isStatic()
      */
     public function isStatic()
     {
@@ -225,10 +207,8 @@ abstract class StorageAbstract
     }
         
     /**
-     * 载入文件以显示
-     * 
-     * @throws \wf\storage\Exception
-     * @param string $path
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::load()
      */
     public function load($path)
     {
@@ -307,42 +287,25 @@ abstract class StorageAbstract
             return true;
         }
     }
-    
-    /**
-     * 获取安全文件路径名
-     * @param string $path
-     * @return string
-     */
-    public function safePath($path)
-    {
-        $path = str_replace('\\', '/', $path);
-        $path = preg_replace('/(\.+\\/)/', './', $path);
-        $path = preg_replace('/(\\/+)/', '/', $path);
-        return $path;
-    }
 
     /**
-     * 读取内容
-     *
-     * @param string $path
-     * @return string
+     * 
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::getContent()
      */
     abstract public function getContent($path);
     
     /**
-     * 存贮附件
      * 
-     * @param string $path
-     * @param string $content
-     * @return bool
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::save()
      */
     abstract public function save($path, $content);
     
     /**
-     * 生成附件路径
-     * @param string $suffix 后缀 xxx
-     * @throws \wf\storage\Exception
-     * @return string
+     * 
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::generatePath()
      */
     public function generatePath($suffix)
     {
@@ -361,26 +324,38 @@ abstract class StorageAbstract
     }
 
     /**
-     * 上传文件
-     * @param string $tempFile
-     * @param string $uploadPath
+     * 
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::upload()
      */
     abstract public function upload($tempFile, $uploadPath);
     
     /**
-     * 复制文件到附件目录
-     * @param string $pathFrom 来源文件完整的路径（注意该文件路径的安全）
-     * @param string $pathTo
-     * @return boolean
+     * 
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::copy()
      */
     abstract public function copy($pathFrom, $pathTo);
     
     /**
-     * 附件是否存在
-     * @param string $path
-     * @return boolean
+     * 
+     * {@inheritDoc}
+     * @see \wf\storage\StorageInterface::isExist()
      */
     abstract public function isExist($path);
+    
+    /**
+     * 获取安全文件路径名
+     * @param string $path
+     * @return string
+     */
+    protected function safePath($path)
+    {
+        $path = str_replace('\\', '/', $path);
+        $path = preg_replace('/(\.+\\/)/', './', $path);
+        $path = preg_replace('/(\\/+)/', '/', $path);
+        return $path;
+    }
 }
 
 
